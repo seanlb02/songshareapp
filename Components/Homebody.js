2676d82fa27cc1de7c2c59f5b62fdc40bf12e2be
Sean <12708@coderacademy.edu.au>
2022-12-07 15:30:04 +1100

    added homepage images

--- Components/Homebody.js
+++ Components/Homebody.js
@@ -2,6 +2,7 @@ import React from'react';
 import ReactDOM from'react-dom';
 import Image from 'next/image';
 import Link from 'next/link';
+import messages from '../public/messages.svg'
 
 
 export default function Homebody() {
@@ -31,24 +32,27 @@ export default function Homebody() {
                 <div className={styles.aboutOneTitle}>
                         New music you add to your profile's <em>track tree</em> gets automatically distributed to your followers as <u>branches</u>.
                 </div>
+                <div className={styles.rightImage}>
+                <Image src="/upload.svg" width={700} height={100}/>
+                </div>
             
             </div>
             <div className={styles.aboutTwo}>
                 <div className={styles.aboutTwoTitle}>
-                    The branches you are sent by the people you follow are stored like a messages feed. Consider these feeds as personalised playlists. 
+                    Branches sent to you by the people you follow are stored like a messages feed. Consider these feeds as personalised playlists. 
                 </div>
-                <div className={styles.messageFeedImage}>
-                <Image/>
+                <div className={styles.rightImage}>
+                <Image src="/messages.svg.svg" width={700} height={100}/>
                 </div>
             </div>
             <div className={styles.aboutThree}>
-                <div className={styles.aboutThreeTitle}>
+                <div className={styles.aboutOneTitle}>
                     Keep your branches in order by <em>removing</em> tracks you dont vibe with. 
                     <br></br>
                     <br></br>Don&apos;t worry &mdash; we won&apos;t tell them
                 </div>
-                <div className={styles.removingTracksImage}>
-                <Image/>
+                <div className={styles.rightImage}>
+                <Image src="/delete.svg" width={700} height={100}/>
                 </div>
             </div>     
     </div>
@@ -56,7 +60,7 @@ export default function Homebody() {
 }
 
 const styles = {
-    HeroContainer: 'flex xs:flex-col lg:flex-row lg:min-h-screen border-b-4 border-black mb-10 ',
+    HeroContainer: 'flex xs:flex-col lg:flex-row lg:min-h-screen mb-10 ',
     HeroText: 'flex flex-col w-2/5  xs:p-10 lg:mt-0 pr-0',
     heroImage: 'flex w-3/5',
     title: 'font-roboto md:pr-0 font-bold xs:text-3xl md:text-4xl md:px-0 xs:pt-12 md:pt-16 md:text-center lg:text-left lg:text-5xl mb-10 lg:pl-4', 
@@ -67,13 +71,12 @@ const styles = {
     loginBox: 'flex w-2/3',
     forgot: 'text-blue-600 text-sm ml-2 mt-6 mb-12 xs:mb-0', 
 
-    aboutOne: 'flex flex-row h-[40vw] w-[100vw]',
+    aboutOne: 'flex flex-row h-[50vw] w-[100vw] mb-36 bg-blue-300 py-12 text-white',
     aboutOneTitle: 'w-1/2 font-roboto  xs:text-2xl md:text-2xl md:px-24 xs:pt-12 md:pt-16 md:text-center lg:text3xl mb-10 lg:pt-44 lg:text-left',
-    addingTracksImage: 'flex w-1/2 ',
-    aboutTwo: 'flex flex-row-reverse h-[50vh] h-[40vw] w-[100vw]',
-    aboutTwoTitle: ' w-1/2 font-roboto lg:pl-24 xs:text-2xl md:text-2xl md:px-24 xs:pt-12 md:pt-16 md:text-center lg:text3xl mb-10 lg:pr-36 lg:pt-44 lg:text-left',
+    rightImage: 'flex w-1/2',
+    aboutTwo: 'flex flex-row-reverse  h-[50vw] w-[100vw] mb-36 py-12 bg-yellow-200',
+    aboutTwoTitle: 'w-1/2 font-roboto  xs:text-2xl md:text-2xl md:px-24 xs:pt-12 md:pt-16 md:text-center lg:text3xl mb-10 lg:pt-44 lg:text-left ',
     messageFeedImage: 'flex w-1/2 ',
-    aboutThree: 'flex flex-row h-[40vw] w-[100vw] ',
-    aboutThreeTitle: ' w-1/2 font-roboto xs:text-2xl md:text-2xl md:px-24 xs:pt-12 md:pt-16 md:text-center lg:text3xl mb-10 lg:pr-52 lg:pt-44 lg:text-left',
-    removingTracksImage: 'flex w-1/2 '
+    aboutThree: 'flex flex-row h-[50vw] w-[100vw] mb-36 bg-red-300 py-12',
+
 }
