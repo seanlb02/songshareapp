9b8324ec206aed6d1bd1ffaabd00cf5bab07ab25
Sean <12708@coderacademy.edu.au>
2022-12-15 12:50:51 +1100

    built friends list rendering

--- Components/FriendListItem.js
+++ Components/FriendListItem.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Link from 'next/link'
 import Image from "next/image";
+import { useState, useEffect } from "react";
 
-export default function FriendListItem() {
+export default function FriendListItem({UserName}) {
+
+
+
+
+
+    
+
   return (
     <div className={styles.container}>
         <img className={styles.profilePic} src="https://i.pravatar.cc/300" height={25} width={25}/>
-        <div className={styles.userName}>User Name</div>
+        <div className={styles.userName}>{UserName}</div>
     </div>
   )
 }
--- Components/FriendsList.js
+++ Components/FriendsList.js
@@ -2,27 +2,60 @@ import React from "react";
 import Link from 'next/link'
 import Image from "next/image";
 import FriendListItem from "./FriendListItem";
+import { useState, useEffect } from "react";
 
 
 export default function FriendsList() {
+
+
+// resolve following list as array of user IDs 
+  // const userToken = localStorage.getItem("tokenKey")
+  const [UserID, setUserID] = useState("")
+ 
+const [followers, setFollowers] = useState()
+let followerArray = []
+
+async function getFollowers() {
+
+      var token = (JSON.parse(localStorage.getItem("tokenKey").replaceAll("", '')))
+      console.log(token)
+      await fetch('http://127.0.0.1:8000/connections/following/', {
+      method: 'GET',
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+          },
+        
+      })
+      .then(res => res.json())
+      .then((data => data.map(object => followerArray.push(object.followee_id))))
+      .then(() => setFollowers(followerArray))
+      .catch((error) => {
+        console.error('Error:', error);})
+     }
+
+  useEffect(() => {
+  
+    
+    getFollowers()
+    
+      
+    }, [])
+
+
+
   return (
     <div className={styles.sideContainer}>
         <div className={styles.search}>
             <input type="text"  className={styles.searchInput} name="Friendsearch" placeholder="Search..."/>
             <button type="submit" className={styles.searchButton}><Image src="/searchIcon.png" width={25} height={25}></Image></button>
         </div>
-        <FriendListItem/>
-        <FriendListItem/>
-        <FriendListItem/>
-        <FriendListItem/>
-        <FriendListItem/>
-        <FriendListItem/>
-        <FriendListItem/>
-        <FriendListItem/>
-        <FriendListItem/>
-        <FriendListItem/>
-        <FriendListItem/>
-        <FriendListItem/>
+        <div>{followerArray}</div>
+        <div>i</div>
+        {followers && followers.map(follower => (<FriendListItem UserName={follower}/>))}
+      
+        
+         
     
     </div>
   )
